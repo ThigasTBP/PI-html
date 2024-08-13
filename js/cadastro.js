4cfd09ca80f8c523ec1282c0814384564a06a5be
ThigasTBP <thiagopessoatbp@gmail.com>
2024-08-13 12:06:06 -0300

    senac

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,87 +1,42 @@
 $(document).ready(function () {
-<<<<<<< HEAD
-    $("#btnlogin").on('click', function () {
-        $('#form').on('click', function (event) {
-            event.preventDefaut()
-        })
-
-        var email = $('#email').val()
-        var password = $('#password').val()
-        var confirm = $('#confim').val()
-
-        if (email != '' && password != '' && confirm != '') {
-            if (password === confirm) {
-                $.ajax({
-                    url: 'http://localhost:3333/user',
-                    method: 'post',
-=======
-    $("#btnsalvar").on("click", function () {
+    $("#btnlogin").on("click", function () {
         $(".form").on("click", function (event) {
             event.preventDefault()
         })
-        var email = $(".email").val()
-        var senha = $(".password").val()
-        var confirm = $(".confirm").val()
+        var email = $("#email").val()
+        var senha = $("#password").val()
+        var confirm = $("#confirm").val()
 
-
+        console.log(email);
+        console.log(senha);
+        console.log(confirm);
+        
+        
+        
 
         if (email != '' && senha != '' && confirm != '') {
             if (senha == confirm) {
-                console.log(email);
-                console.log(senha);
-                console.log(confirm);
                 $.ajax({
                     url: 'http://localhost:3333/user',
                     method: 'POST',
->>>>>>> a4029d012b1e4f114310ab02bb5aa08838e97b64
                     cache: false,
                     dataType: 'json',
                     data: {
                         email: email,
-<<<<<<< HEAD
-                        senha: password
-                    },
-                    success: function () {
-                        alert('usuario cadastrado')
-                        $.ajax({
-                            url: 'http://localhost:3333/login',
-                            method: 'post',
-                            cache: false,
-                            dataType: 'json',
-                            data: {
-                                email: email,
-                                senha: password
-                            }
-                        })
-                        $('#form').each(function () {
-                            this.reset()
-                        })
-=======
                         senha: senha
                     },
-
                     success: function () {
                         alert(' usuario adicionado com sucesso')
->>>>>>> a4029d012b1e4f114310ab02bb5aa08838e97b64
                     },
                     error: function (error) {
                         console.log(error)
                     }
                 })
             } else {
-<<<<<<< HEAD
                 alert('as senhas devem ser iguais')
             }
         } else {
             alert('preencha corretamente')
         }
-
-=======
-                alert('Preencha corretamente')
-            }
-        }
-
-
->>>>>>> a4029d012b1e4f114310ab02bb5aa08838e97b64
     })
 })
--- js/criacao.js
+++ js/criacao.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-    var $select1 = $("#selecao")
+    var forca = $('#forca').val()
+    var des = $('#destreza').val()
+    var con = $('#constituicao').val()
+    var int = $('#inteligencia').val()
+    var sab = $('#sabedoria').val()
+    var car = $('#carisma').val()
+    var nome = $('#nome').val()
+    var classe = $('#classe').val()
+    var raca
+
+
+    var $select1 = $("#selecao1")
     $select1.empty()
     $.ajax({
         url: 'http://localhost:3333/raca',
@@ -13,10 +24,28 @@ $(document).ready(function () {
             console.log(error)
         }
     })
+
+    var $select2 = $("#selecao2")
+    $select2.empty()
+    $.ajax({
+        url: 'http://localhost:3333/classe',
+        method: 'get',
+        success: function (result) {
+            $.each(result, function (index, item) {
+                $select2.append('<button class="btnclasse" type="button" id="' + item.cod_classe + '">' + item.nome + '</button>')
+            })
+        },
+        error: function (error) {
+            console.log(error);
+
+        }
+    })
+
     $(document).on('click', '.btnraca', function () {
         var $descrition = $("#descricao")
         $descrition.empty()
         var id = $(this).attr('id')
+        raca = id
         $.ajax({
             url: 'http://localhost:3333/raca/' + id,
             method: 'get',
@@ -24,10 +53,27 @@ $(document).ready(function () {
                 $descrition.html("<h1>" + result.nome + "</h1> <hr>")
                 if (result.forca == null || result.destreza == null || result.constituicao == null || result.sabedoria == null || result.inteligencia == null || result.carisma == null) {
                     $descrition.append("<h3> +2 em três atributos diferentes </h3>")
-                }else{
-                    $descrition.append("<h3> força: "+result.forca+" <br> destreza: "+result.destreza+" <br> constituição: "+result.constituicao+" <br> sabedoria: "+result.sabedoria+" <br> inteligencia: "+result.inteligencia+" <br> carisma: "+result.carisma+" <br></h3>")
+                } else {
+                    $descrition.append("<h3> força: " + result.forca + " <br> destreza: " + result.destreza + " <br> constituição: " + result.constituicao + " <br> sabedoria: " + result.sabedoria + " <br> inteligencia: " + result.inteligencia + " <br> carisma: " + result.carisma + " <br></h3>")
                 }
-                $descrition.append("<h3> tamanho: "+result.tamanho+" <br> deslocamento: "+result.deslocamento+" metros </h3>")
+                $descrition.append("<h3> tamanho: " + result.tamanho + " <br> deslocamento: " + result.deslocamento + " metros </h3>")
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        })
+    })
+    $(document).on('click', '.btnclasse', function () {
+        var $descrition = $("#descricao")
+        $descrition.empty()
+        var id = $(this).attr('id')
+        $.ajax({
+            url: 'http://localhost:3333/classe/' + id,
+            method: 'get',
+            success: function (result) {
+                $descrition.html("<h1>" + result.nome + "</h1> <hr>")
+                $descrition.append("<h3> vida: " + result.vida + " + "+result.vida_nivel+" por nivel <br> mana: " + result.mana + " por nivel <br> proficiencias: "+result.proficiencia_arma+", "+result.proficiencia_armadura+", "+result.proficiencia_escudo+" </h3>")
+                
             },
             error: function (error) {
                 console.log(error)
